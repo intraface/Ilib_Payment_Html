5719f9e479e6319b7fda9f9978b5f15d3857fe0f
Sune T. Jensen <sj@sunet.dk>
2008-06-16 20:35:21 +0000

    Removed constructor from whitelist
    git-svn-id: http://svn.intraface.dk/intrafacepublic/3Party/Ilib_Payment_Html@986 2fc64ab5-e821-0410-98e1-ee8f871d7b73

--- tests/PostprocessTest.php
+++ tests/PostprocessTest.php
@@ -1,10 +1,10 @@
 <?php
 require_once 'PHPUnit/Framework.php';
-require_once 'k/controller.php';
+
 
 PHPUnit_Util_Filter::addDirectoryToWhitelist(realpath(dirname(__FILE__) . '/../src/'));
-
-
+PHPUnit_Util_Filter::removeDirectoryFromWhitelist(realpath(dirname(__FILE__) . '/../src/Ilib/Payment/Html/Controller/'));
+PHPUnit_Util_Filter::removeDirectoryFromWhitelist(realpath(dirname(__FILE__) . '/../src/Ilib/Payment/Html/templates'));
 
 
 class PostprocessTest extends PHPUnit_Framework_TestCase
