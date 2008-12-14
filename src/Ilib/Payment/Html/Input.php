948f051f053fdea3d2441dd278d25af3fc8c915e
Lars Olesen <lars@intraface.dk>
2008-12-14 18:23:37 +0000

    Added method to getCompanyInformation()
    git-svn-id: http://svn.intraface.dk/intrafacepublic/3Party/Ilib_Payment_Html@1402 2fc64ab5-e821-0410-98e1-ee8f871d7b73

--- src/Ilib/Payment/Html/Input.php
+++ src/Ilib/Payment/Html/Input.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * To control input page for online payments
- * 
+ *
  * @author sune jensen <sj@sunet.dk>
  * @version 0.0.1
  * @package Payment_Html_Provider
@@ -13,21 +13,21 @@ class Ilib_Payment_Html_Input
     /**
      * @var string merchant number
      */
-    protected $merchant; 
-    
+    protected $merchant;
+
     /**
      * @var string verification key
      */
     protected $verification_key;
-    
+
     /**
      * @var string session_id
      */
     protected $session_id;
-    
+
     /**
      * Constructor
-     * 
+     *
      * @param string $merchant merchant number
      * @param string $verification_key verification key
      * @param string $session_id session id
@@ -35,49 +35,50 @@ class Ilib_Payment_Html_Input
     public function __construct($merchant, $verification_key, $session_id)
     {
         $this->merchant = $merchant;
-        $this->verification_key = $verification_key; 
+        $this->verification_key = $verification_key;
         $this->session_id = $session_id;
     }
-    
+
     /**
      * Returns a path to a input template matching the provider.
      * Should be replaced in local classes
-     * 
+     *
      * @return string template path
      */
-    public function getInputTemplatePath() 
+    public function getInputTemplatePath()
     {
         return false;
     }
-    
+
     /**
      * Returns the url to set in front of local urls, to make it secured
      * Should be replaced in local classes
-     * 
+     *
      * @return string secure tunnel url
      */
     public function getSecureTunnelUrl()
     {
         return false;
     }
-    
+
     /**
      * returns merchant number
-     * 
+     *
      * @return string merchant number
      */
-    public function getMerchant() 
+    public function getMerchant()
     {
         return $this->merchant;
     }
-    
+
     /**
      * returns the verification key
-     * 
+     *
      * @return string verification key
      */
     public function getVerificationKey()
     {
         return $this->verification_key;
     }
+
 }
